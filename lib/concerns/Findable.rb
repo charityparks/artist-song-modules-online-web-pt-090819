10cd737c6b93698794c39b53ceb1fabafa09f70b
Charity Parks <tuppercharity@gmail.com>
2019-10-16 23:35:52 +0000

    Automatically backed up by Learn

--- lib/concerns/Findable.rb
+++ lib/concerns/Findable.rb
@@ -2,7 +2,8 @@ module Findable
 
   module ClassMethods
     def self.find_by_name(name)
-      @@artists.detect d{|a| a.name == name}
+      @@artists.detect do |a| a.name == name
+    end
     end
   end
 
